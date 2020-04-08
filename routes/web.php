a2036e2d2643164a00f9733afd9da09b40d0a708
Prozis <prozius@tut.by>
2020-04-08 21:42:11 +0300

    add main page, add routs

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/articles', function () {
+    $articles = App\Article::all();
+    return view('articles', ['articles' => $articles]);
+});
